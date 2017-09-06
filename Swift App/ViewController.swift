167b7a594c50d840b46dc28bd8b8eb4fe6286a68
Grant Marks <grant@Grants-MacBook-Air-2.local>
2017-09-05 21:40:54 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Grant Marks on 9/5/17.
+//  Copyright © 2017 Grant Marks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
