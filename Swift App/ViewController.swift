2d4e3f1c4301e22b0acfa71dd705939fe5e6be26
Grant Marks <grant@Grants-MacBook-Air-2.local>
2017-09-06 10:13:24 -0500

    Fourth commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -12,16 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+
+    @IBOutlet weak var text1: UITextField!
+
     
-    @IBAction func buttonTapped(_ sender: Any) {
-        tapCount = tapCount + 1
-        print(tapCount)
-        if tapCount >= 10 {
-            print("You have pushed the button 10 times!!!")
-            theLabel.text = "You tapped the button 10 times!"
-        }
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        print(text1.text!)
+        print(text2.text!)
+        theLabel.text = "The answer is...\(Double(text1.text!)! + Double(text2.text!)!)"
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
